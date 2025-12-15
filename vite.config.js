3ac369984bb759f8f718d4b0bfb52a22fcb253fd
G-Baudelaire <germainjones1@gmail.com>
2025-12-15 02:44:40 +0000

    Update url

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/age-and-gender-prediction/",
+  base: "/age-prediction/",
   build: {
     outDir: "docs",
   },
